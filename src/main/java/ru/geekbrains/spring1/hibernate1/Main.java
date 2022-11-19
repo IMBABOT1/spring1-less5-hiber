7afae7ba1b69cb48ae43a509333e16768e31139d
unknown <zlovred12@gmail.com>
2022-11-19 21:58:45 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/spring1/hibernate1/Main.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.spring1.hibernate1;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SessionFactoryUtils sessionFactoryUtils = new SessionFactoryUtils();
+        sessionFactoryUtils.init();
+
+        ProductDao productDao = new ProductDaoImpl(sessionFactoryUtils);
+
+        Product p = productDao.findById(1l);
+        System.out.println(p);
+
+        productDao.saveOrUpdate(new Product(1l, "dfas", 587));
+
+        List<Product> productList = productDao.findAll();
+        System.out.println(productList);
+
+        sessionFactoryUtils.shutdown();
+    }
+}
